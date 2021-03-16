a44b9c1a897efaf81a8c28e07bd57589044051cb
T20210311 <fslongjin@vip.qq.com>
2021-03-16 11:21:20 +0800

    更新了画函数图的代码
    更新了画函数图的代码

--- python/画函数图.py
+++ python/画函数图.py
@@ -2,26 +2,30 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-x = np.linspace(0.0, 1.5, 1000)
-y = 1/(2*x)
+
+x = np.linspace(-5, 5, 1000000)
+y = np.sqrt(2*x - x**2)
+
+
+
 
 # 设置图像大小
-plt.figure(num=3, figsize=(15, 10))
+plt.figure(num=3, figsize=(10, 10))
 
 # 绘图
 plt.plot(x, y, color='red', linewidth=1.0, linestyle='-')
 
 # 设置坐标轴标签、范围
-plt.xlim(0, 1)
-plt.ylim(0, 15)
+plt.xlim(0, 2.5)
+plt.ylim(0, 2.5)
 plt.xlabel('x')
 plt.ylabel('y')
 
 # 设置刻度范围及数量
-new_ticks = np.linspace(0, 1.5, 10)
+x_ticks = np.linspace(0, 2.5, 10)
 
-plt.xticks(new_ticks)
-y_ticks = np.linspace(0, 10, 20)
+plt.xticks(x_ticks)
+y_ticks = np.linspace(0, 2.5, 10)
 plt.yticks(y_ticks)
 
 # 获取坐标轴信息
@@ -42,7 +46,7 @@ ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data', 0))
 
 # 设置标签
-ax.set_title('xy=1/2', fontsize=14, color='r')
+ax.set_title('y=sqrt(2*x - x**2)', fontsize=14, color='r')
 
 plt.show()
 
